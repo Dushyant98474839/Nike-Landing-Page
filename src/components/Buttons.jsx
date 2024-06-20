67d577d67ab82f45cb8958faf71d187e5e0cd946
Dushyant98474839 <118445519+Dushyant98474839@users.noreply.github.com>
2024-06-20 10:53:44 +0530

    Add files via upload

---
+++ src/components/Buttons.jsx
@@ -0,0 +1,13 @@
+
+const Buttons = ({label, iconURL}) => {
+  return (
+    <button className="flex justify-center items-center leading-none gap-2 px-7 py-4 border font-montserrat text-lg bg-coral-red rounded-full text-white border-coral-red">
+      {label}
+    <img src={iconURL} className="ml-2 rounded-full w-5 h-5"
+
+    />
+    </button>
+  )
+}
+
+export default Buttons
---
+++ src/sections/CustomerReviews.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const customerReviews = () => {
+  return (
+    <div>CustomerReviews</div>
+  )
+}
+
+export default customerReviews
